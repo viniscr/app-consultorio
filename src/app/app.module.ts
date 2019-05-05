faea0c77f8cce70f5d054f5d455a78f3df6569de
Vinícius Barbosa <alencarvi@gmail.com>
2019-05-04 23:10:31 -0300

    componentes painel, barra de navegacao e resumo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
+import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
+import { MenuLateralComponent } from './barra-navegacao/menu-lateral/menu-lateral.component';
+import { ResumoComponent } from './resumo/resumo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelSimplesComponent,
+    BarraNavegacaoComponent,
+    MenuLateralComponent,
+    ResumoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/painel-simples/painel-simples.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-painel-simples',
+  templateUrl: './painel-simples.component.html',
+  styleUrls: ['./painel-simples.component.css']
+})
+export class PainelSimplesComponent implements OnInit {
+
+  @Input() titulo;
+  @Input() col;
+  @Input() tipo = "info";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
