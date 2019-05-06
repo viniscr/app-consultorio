092cc9612fa1f7d51e5c93a24e5c967e4a2e7cc7
Vinícius Barbosa <alencarvi@gmail.com>
2019-05-05 21:49:40 -0300

    Manipulando classes do HTML

--- src/app/resumo/resumo.component.ts
+++ src/app/resumo/resumo.component.ts
@@ -7,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumoComponent implements OnInit {
 
+  resumo = {
+    consultas: {
+      anteriores: {
+        total: 87,
+        detalhes: [
+          { especialidade: "Cardiologia", quantidade: 20 },
+          { especialidade: "Clínica Geral", quantidade: 30 },
+        ]
+      },
+      marcadas: {
+        total: 79,
+        detalhes: [
+          { especialidade: "Cardiologia", quantidade: 19 },
+          { especialidade: "Clínica Geral", quantidade: 28 },
+        ]
+      }
+    },
+    faturamento: {
+      anterior: {
+        valor: 100000,
+        comparativo: 19
+      },
+      previsao: {
+        valor: 90000,
+        comparativo: -10
+      }
+    }
+  };
+
   constructor() { }
 
   ngOnInit() {
